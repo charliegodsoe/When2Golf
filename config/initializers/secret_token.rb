0ab9f9737ddd9183bcadad418988c106bc5c1c34
Charlie Godsoe <charliegodsoe@gmail.com>
2010-09-15 18:40:46 -0600

    The first pass on the object models and scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+When2Golf::Application.config.secret_token = '014e6a017913d7f6f2abac6163df5d75bfdb2dd20635bdb77a78910fa5af93a20508165149672871b819f78fe8e619b82120d2537bc7b721944ce009140e744c'
---
+++ db/migrate/20100915231427_create_user_roles.rb
@@ -0,0 +1,20 @@
+class CreateUserRoles < ActiveRecord::Migration
+  def self.up
+    create_table :user_roles do |t|
+      t.string :role
+      t.string :description
+      t.timestamps
+    end
+    
+    UserRole.reset_column_information
+    
+    UserRole.create :role => "Owner", :description => "Manages courses and course information along with Publishers"
+    UserRole.create :role => "Publisher", :description => "Manages course content and tee time availablity information"
+  end
+
+  def self.down
+    drop_table :user_roles
+  end
+  
+  
+end
